ae63c943f11705d4bc2ae8347e15ce27de5dd52a
Caroline Lukens <clukens91@gmail.com>
2022-07-08 17:17:02 -0400

    created promptPlayer() func to loop prompt until user enters valid input

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-// computer rolls
-// default behavior is to return string result
+// prompt computer for roll
+// defaults to returning string
 // set param returnInt = true to return raw integer roll instead
 //    (for testing probabilities or etc)
 // returns string "rock" "paper" or "scissors"
-function computerPlay(returnInt = false) {
+function promptComputer(returnInt = false) {
   let roll = Math.floor(Math.random()*3);
 
   // return raw integer instead
@@ -12,16 +12,114 @@ function computerPlay(returnInt = false) {
   
     // return string "rock", "paper" or "scissors"
   if(roll === 0)
-    return "rock";
+    return `rock`;
   else if(roll === 1)
-    return "paper";
+    return `paper`;
   else
-    return "scissors";
+    return `scissors`;
 }
 
+// Play a round of rock, paper, scissors
+// return "error", "tie", "win" or "lose"
+function playRound(playerSel, compSel) {
+  // prep selections
+  //if(!playerSel) playerSel = promptPlayer();
+  //if(!compSel) compSel = computerPlay();
+
+  // error condition if player didn't pick rock/paper/scissors
+  if(playerSel !== "rock" && playerSel !== "paper" && playerSel !== "scissors") {
+    // log error string `Error: Input "${playerSelInput}", expecting "rock", "paper" or "scissors".`
+     return `error`;
+  }
+
+  // tie condition
+  if(playerSel === compSel) return `tie`;
+  
+  // player win conditions
+  if((playerSel === "rock" && compSel === "scissors") || 
+    (playerSel === "paper" && compSel === "rock") || 
+    (playerSel === "scissors" && compSel === "paper")) {
+    return `win`;
+  }
+
+  // player lose conditions
+  if((playerSel === "rock" && compSel === "paper") || 
+    (playerSel === "paper" && compSel === "scissors") || 
+    (playerSel === "scissors" && compSel === "rock")) {
+    return `lose`;
+  }
+
+  // execution shouldn't get here, but just in case
+  // log error string `Error: Unexpected result!`
+  return `error`;
+}
+
+// prompts user for selection
+// handles input sanitizing; repeats prompt until receiving valid input
+// returns string "rock", "paper" or "scissors"
+function promptPlayer() {
+  let validInput = false;
+  let playerInputRaw, playerInput;
+  let i = 0;
+
+  // simplify by just returning out of the loop?
+  while(validInput === false) {
+    console.log(`Prompt #${i++}  `);
+    playerInputRaw = prompt(`Type "rock", "paper" or "scissors":`);
+    playerInput = playerInputRaw.toLowerCase();
+
+    if(playerInput === "rock" || 
+    playerInput === "paper" || 
+    playerInput === "scissors") {
+      validInput = true;
+    }
+  }
+
+  return playerInput;
+}
+
+// Run game for a number of rounds (default 5) and return a winner
+function game(rounds = 5) {
+  let playerWins = 0, compWins = 0, ties = 0, errors = 0;
+  let roundResult, gameResult;
+
+  for(let i = 0; i < rounds; i++) {
+    // get computer and player choices
+    roundResult = playRound(promptPlayer(), promptComputer());
+
+    switch(roundResult) {
+      case "tie":
+        ties++;
+        break;
+      case "win":
+        playerWins++;
+        break;
+      case "lose":
+        compWins++;
+        break;
+      case "error":
+        errors++;
+        break;
+      default:
+        errors++;
+    }
+
+    console.log(roundResult);
+  }
+
+  console.log(`Wins: ${playerWins} Losses: ${compWins} Ties: ${ties} Errors: ${errors}`);
+
+  if(playerWins === compWins) gameResult = `tie`;
+  else if(playerWins > compWins) gameResult = `win`;
+  else gameResult = `lose`;
+}
+
+game();
+
+// OLD VERSION
 // Play a round of rock, paper, scissors and return a string with the result text
 // if returnSimple = true instead returns "error", "tie", "win" or "lose"
-function playRound(playerSelInput, compSel, returnSimple = false) {
+/*function playRound(playerSelInput, compSel, returnSimple = false) {
   // prep selections
   let playerSel = playerSelInput.toLowerCase();
   if(!compSel) compSel = computerPlay();
@@ -49,46 +147,12 @@ function playRound(playerSelInput, compSel, returnSimple = false) {
     return (returnSimple ? `lose` : `You lost! Computer picked ${compSel} which beat your ${playerSel}!`);
   }
 
-  // shouldn't get here, but just in case
+  // execution shouldn't get here, but just in case
   return (returnSimple ? `error` : `Error: Unexpected result!`);
-}
+}*/
 
-// Run game for a number of rounds (default 5) and return a winner
-function game(rounds = 5) {
-  let playerWins = 0, compWins = 0, ties = 0, errors = 0;
-  let playerInput, compInput, gameResult;
 
-  for(let i = 0; i < rounds; i++) {
-    // get computer and player choices
-    compInput = computerPlay();
-    playerInput = prompt(`Type "rock", "paper" or "scissors":`);
 
-    gameResult = playRound(playerInput, compInput, true);
-
-    switch(gameResult) {
-      case "tie":
-        ties++;
-        break;
-      case "win":
-        playerWins++;
-        break;
-      case "lose":
-        compWins++;
-        break;
-      case "error":
-        errors++;
-        break;
-      default:
-        errors++;
-    }
-
-    console.log(gameResult);
-  }
-
-  console.log(`Wins: ${playerWins} Losses: ${compWins} Ties: ${ties} Errors: ${errors}`);
-}
-
-game();
 
 /*function playRound(playerSelStr, compSel) {
   let playerSelLower = playerSelString.toLowerCase();
@@ -122,7 +186,7 @@ function testRolls(totalRolls) {
   let rErr = 0;
 
   for(let i = 0; i < totalRolls; i++) {
-    rollResult = computerPlay(true);
+    rollResult = promptComputer(true);
 
     if(rollResult === 0) {
       r0++;
